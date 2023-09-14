8f55975dbaa1dfb373aa2970047b898d83cc6452
Ryan Hinds <115953645+FossRyanH@users.noreply.github.com>
2023-09-14 14:58:19 -0600

    Basic movement complete, gravity feels a little weird, adding animations next.

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,15 +3,24 @@ using System;
 
 public partial class PlayerController : CharacterBody3D
 {
-	public float walkSpeed = 3.41f;
-	public float sprintSpeed = 12f;
-	public float jumpForce = 7f;
+	Node3D cameraMount;
+
+	public static float walkSpeed = 3.41f;
+	public static float sprintSpeed = 9f;
+	public float jumpForce = 4.5f;
+	int jumpCount = 0;
+
+	static float moveSpeed = walkSpeed;
 
 	[Export] float sensitivity = 0.085f;
+
+	[Export] float gravity = 9.81f;
 
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+		cameraMount = GetNode<Node3D>("CameraMount");
 	}
 
 	public override void _Input(InputEvent @event)
@@ -19,15 +28,52 @@ public partial class PlayerController : CharacterBody3D
 		if (@event is InputEventMouseMotion mouseMotion)
 		{
 			RotateY(Mathf.DegToRad(-mouseMotion.Relative.X * sensitivity));
+			cameraMount.RotateX(Mathf.DegToRad(-mouseMotion.Relative.Y * sensitivity));
 		}
 	}
 
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	[Export] float gravity = 9.81f;
-
 	public override void _PhysicsProcess(double delta)
 	{
-		// 
+		Vector3 velocity = Velocity;
+
+		if (!IsOnFloor())
+		{
+			velocity.Y -= gravity * (float)delta;
+		}
+
+		if (Input.IsActionJustPressed("jump") && jumpCount < 1)
+		{
+			velocity.Y = jumpForce;
+		}
+		else
+		{
+			jumpCount = 0;
+		}
+
+		if (Input.IsActionPressed("sprint"))
+		{
+			moveSpeed = sprintSpeed;
+		}
+		else if (Input.IsActionJustReleased("sprint"))
+		{
+			moveSpeed = walkSpeed;
+		}
+
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0 , inputDir.Y)).Normalized();
+
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * moveSpeed;
+			velocity.Z = direction.Z * moveSpeed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0 ,moveSpeed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0 ,moveSpeed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
